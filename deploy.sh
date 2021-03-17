4638fee22edfb511a3165f5ad478f7e359cf07d9
root <root@ip-172-31-11-32.us-west-1.compute.internal>
2021-03-17 08:44:18 +0000

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,10 @@ echo "deploy start"
 
 git pull
 
-docker build -t gemcardweb .
+docker build -t auction .
 
-docker stop gemcardweb1
-docker run -idt -p 8000:8000 --rm --name gemcardweb1 gemcardweb
-
-sleep 7
-
-docker stop gemcardweb2
-docker run -idt -p 8001:8000 --rm --name gemcardweb2 gemcardweb
+docker stop auction1
+docker run -idt -p 8000:8000 --rm --name auction1 auction
 
 docker image prune -f
 echo "deploy end"
